55623fd0799b875e27ccf08ee71afbe62fa71c98
Mephisto <mephistowaltz22@gmail.com>
2022-10-26 11:59:40 +0900

    Lv.7 Q10809

--- Lv.7/Q10809.c
+++ Lv.7/Q10809.c
@@ -1,4 +1,30 @@
 #include <stdio.h>
 
-int main() {
+int main()
+{
+	int i = 'a';
+	int j;
+	int flag;
+	char str[100];
+
+	scanf("%s", str);
+
+	while (i <= 'z')
+	{
+		flag = 0;
+		j = 0;
+		while (str[j])
+		{
+			if (str[j] == i)
+			{
+				printf("%d ", j);
+				flag = 1;
+				break;
+			}
+			j++;
+		}
+		if (flag == 0)
+			printf("-1 ");
+		i++;
+	}
 }
